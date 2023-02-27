d37a538a3fbc688498bb103716aad2cc0a0e1b6c
thao.vu <thaovu@miu.edu>
2023-02-27 12:37:15 -0600

    added question

---
+++ question.js
@@ -0,0 +1,20 @@
+/**
+Create a function findMatching() that accepts 
+two arrays of numbers of different sizes. 
+The function returns an array of numbers that exist in both arrays. 
+Keep in mind that the returned array should not contain duplicates. 
+
+Example: 
+
+const arr1 = [1, 2, 3, 4, 4, 5] 
+
+const arr2 = [3, 4, 5] 
+
+findMatching(arr1, arr2) // returns [ 3, 4, 5 ] 
+
+*/
+/**
+ * Your Name: 
+ */
+
+// Your answer here
